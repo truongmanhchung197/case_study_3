ed2052cc907bce5f98d2b5c14fef3d63c7c55a12
truong manh chung <truongmanhchung207@gmail.com>
2020-11-08 16:41:11 +0700

    Update PostService.java

--- src/main/java/service/PostService.java
+++ src/main/java/service/PostService.java
@@ -1,37 +1,48 @@
 package service;
 
-import javafx.geometry.Pos;
+import com.mysql.cj.jdbc.result.UpdatableResultSet;
 import model.Post;
-import model.User;
+import sun.awt.image.ImageWatched;
 
 import java.sql.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 import java.util.List;
 
-public class PostService implements IPostService{
+public class PostService implements IPostService {
 
     Connection connection = ConnectionDB.getConnection();
     private static final String INSERT_POST_SQL = "INSERT INTO POST(idUser,image,content,timePost) values (?,?,?,?)";
-    private static final String SELECT_POST_SQL = "SELECT * FROM post WHERE idPost = ?;";
-    private static final String SELECT_ALL_POST_SQL = "SELECT * FROM post;";
-    private static final String ALL_POST_SQL = "select u.avatar avt, u.name name, p.timePost time, p.image img, p.content context, count(pl.idPost) likeCount\n" +
+    //    private static final String SELECT_POST_SQL = "SELECT * FROM post WHERE idPost = ?;";
+    //    hiển thị tất cả bài đăng trên trang chủ
+    private static final String SELECT_ALL_POST_HOME_SQL = "select p.idPost id, u.avatar avt, u.name name, p.timePost timePost, p.image img, p.content content, count(pl.idPost) likeCount\n" +
             "from Post p\n" +
             "         left join user u on p.idUser = u.idUser\n" +
             "         left join PostLike pl on p.idPost = pl.idPost\n" +
-            "where u.userAcc = ?";
+            "group by p.idPost, time\n" +
+            "order by time desc;";
+    //    hiển thị tất cả bài đăng trên trang cá nhân
+    private static final String SELECT_ALL_POST_PERSONAL_SQL = "select p.idPost id, u.avatar avt, u.name name, p.timePost timePost, p.image img, p.content content, count(pl.idPost) likeCount\n" +
+            "from Post p\n" +
+            "         left join user u on p.idUser = u.idUser\n" +
+            "         left join PostLike pl on p.idPost = pl.idPost\n" +
+            "where u.userAcc = ?" +
+            "group by p.idPost, time\n" +
+            "order by time desc;";
+    private static final String DELETE_POST_SQL = "delete from Post where idPost = ?";
+    private static final String UPDATE_POST_SQL = "update Post set content = ? where idPost = ?"
 
     @Override
-    public boolean insertPost(Post post,int idUser) {
+    public boolean insertPost(Post post, int idUser) {
         PreparedStatement ps = null;
         int check = 0;
         try {
             ps = connection.prepareStatement(INSERT_POST_SQL);
-            ps.setInt(1,idUser);
-            ps.setString(2,post.getImage());
-            ps.setString(3,post.getContent());
+            ps.setInt(1, idUser);
+            ps.setString(2, post.getImage());
+            ps.setString(3, post.getContent());
             LocalDateTime localDateTime = LocalDateTime.now();
             LocalDate localDate = localDateTime.toLocalDate();
             java.sql.Date date = java.sql.Date.valueOf(localDate);
@@ -43,51 +54,83 @@ public class PostService implements IPostService{
         return check > 0;
     }
 
+//    @Override
+//    public Post selectPost(int idPost) {
+//        try {
+//            PreparedStatement ps = connection.prepareStatement(SELECT_POST_SQL);
+//            ps.setInt(1,idPost);
+//            ResultSet resultSet = ps.executeQuery();
+//            if (resultSet.next()){
+//                String image = resultSet.getString("image");
+//                String content = resultSet.getString("content");
+//                Date date = resultSet.getDate("timePost");
+//                LocalDateTime localDateTime = new java.sql.Timestamp(date.getTime()).toLocalDateTime();
+//                return new Post(idPost,image,content,localDateTime);
+//            }
+//        } catch (SQLException throwables) {
+//            throwables.printStackTrace();
+//        }
+//        return null;
+//    }
+
     @Override
-    public Post selectPost(int idPost) {
+    public LinkedHashMap<Integer, Post> selectAllPostHome() {
+        LinkedHashMap<Integer, Post> listMap = new LinkedHashMap<>();
         try {
-            PreparedStatement ps = connection.prepareStatement(SELECT_POST_SQL);
-            ps.setInt(1,idPost);
+            PreparedStatement ps = connection.prepareStatement(SELECT_ALL_POST_HOME_SQL);
             ResultSet resultSet = ps.executeQuery();
-            if (resultSet.next()){
-                String image = resultSet.getString("image");
-                String content = resultSet.getString("content");
+            while (resultSet.next()) {
+                Integer idPost = resultSet.getInt("idPost");
+                String avatar = resultSet.getString("avt");
+                String userName = resultSet.getString("name");
                 Date date = resultSet.getDate("timePost");
                 LocalDateTime localDateTime = new java.sql.Timestamp(date.getTime()).toLocalDateTime();
-                return new Post(idPost,image,content,localDateTime);
-            }
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
-        return null;
-    }
-
-    @Override
-    public List<Post> selectAllPost() {
-        List<Post> list = new ArrayList<>();
-        try {
-            PreparedStatement ps = connection.prepareStatement(SELECT_ALL_POST_SQL);
-            ResultSet resultSet = ps.executeQuery();
-            while (resultSet.next()){
                 String image = resultSet.getString("image");
                 String content = resultSet.getString("content");
+                Integer likeCount = resultSet.getInt("likeCount");
+                listMap.put(idPost, new Post(avatar, userName, localDateTime, image, content, likeCount));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return listMap;
+    }
+
+    @Override
+    public LinkedHashMap<Integer, Post> selectAllPostPersonal() {
+        LinkedHashMap<Integer, Post> listMap = new LinkedHashMap<>();
+        try {
+            PreparedStatement ps = connection.prepareStatement(SELECT_ALL_POST_PERSONAL_SQL);
+            ResultSet resultSet = ps.executeQuery();
+            while (resultSet.next()) {
+                Integer idPost = resultSet.getInt("idPost");
+                String avatar = resultSet.getString("avt");
+                String userName = resultSet.getString("name");
                 Date date = resultSet.getDate("timePost");
                 LocalDateTime localDateTime = new java.sql.Timestamp(date.getTime()).toLocalDateTime();
-                list.add(new Post(image,content,localDateTime));
+                String image = resultSet.getString("image");
+                String content = resultSet.getString("content");
+                Integer likeCount = resultSet.getInt("likeCount");
+                listMap.put(idPost, new Post(avatar, userName, localDateTime, image, content, likeCount));
             }
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
-        return list;
+        return listMap;
     }
 
     @Override
-    public boolean deletePost(int id) throws SQLException {
-        return false;
+    public void deletePost(int idPost) throws SQLException {
+        PreparedStatement ps = connection.prepareStatement(DELETE_POST_SQL);
+        ps.setInt(1, idPost);
+        ps.executeUpdate();
     }
 
     @Override
-    public boolean updatePost(Post post) throws SQLException {
-        return false;
+    public void updatePost(Post post, int idPost) throws SQLException {
+        PreparedStatement ps = connection.prepareStatement(UPDATE_POST_SQL);
+        ps.setString(1, post.getContent());
+        ps.setInt(2, idPost);
+        ps.executeUpdate();
     }
 }
