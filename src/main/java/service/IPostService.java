2eaf65273ddba1d98886c16d32a0e7ca0a0aee5a
truong manh chung <truongmanhchung207@gmail.com>
2020-11-08 16:40:13 +0700

    Update IPostService.java

--- src/main/java/service/IPostService.java
+++ src/main/java/service/IPostService.java
@@ -1,5 +1,6 @@
 package service;
 
+import javafx.geometry.Pos;
 import model.Post;
 import model.User;
 
@@ -9,8 +10,9 @@ import java.util.List;
 public interface IPostService {
 
     boolean insertPost(Post post,int idUser) throws SQLException;
-    Post selectPost(int idPost);
-    List<Post> selectAllPost();
+//    Post selectPost(int idPost);
+    List<Post> selectAllPostHome();
+    List<Post> selectAllPostPersonal();
     boolean deletePost(int id) throws SQLException;
     boolean updatePost(Post post) throws SQLException;
 
